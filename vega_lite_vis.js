7d50aceb04cd390310848bcfc6c90c69fc381397
JackyZhaoMonash <129266503+JackyZhaoMonash@users.noreply.github.com>
2024-10-11 00:03:45 +1100

    increase chart number

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,20 @@
 var vg_1 = "Week_10.vg.json"; 
-vegaEmbed("#bar_chart", vg_1).then(function(result) { 
+vegaEmbed("#map", vg_1).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_2 = "chart.vg.json"
+vegaEmbed("#line_chart", vg_2).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_3 = "chart2.vg.json"
+vegaEmbed("#bar_chart", vg_3).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_4 = "chart3.vg.json"
+vegaEmbed("#heatmap", vg_4).then(function(result) { 
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
+}).catch(console.error);
+var vg_5 = "chart4.vg.json"
+vegaEmbed("#pie_chart", vg_5).then(function(result) { 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view 
 }).catch(console.error);
